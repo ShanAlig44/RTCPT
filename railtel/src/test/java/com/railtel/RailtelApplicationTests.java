2baf4e043c75005a69ff921756b5d49d0bae89e1
shanalig <md.shan44@gmail.com>
2020-04-24 12:05:43 +0530

    Boot Project structure

---
+++ railtel/src/test/java/com/railtel/RailtelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.railtel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailtelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
